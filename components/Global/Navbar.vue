0433de6cd4dedd6a0a426deef1ef94025dcd95d8
Nilesh Yadav <nileshyadav.dev@gmail.com>
2023-03-09 19:31:29 +0530

    Header and Hero Section Update

--- components/Global/Navbar.vue
+++ components/Global/Navbar.vue
@@ -1,15 +1,68 @@
 <template>
-  <div class="bg-info">
-    <h4 class="text-center text-white">HandyCart navbar</h4>
+  <div>
+   <div class="header">
+    <div class="container ">
+      <div class="row">
+        <div class="nav__wrapper w-100 ">
+          <div class="logo">
+           <LazyImage 
+ :src="require(`~/assets/images/eco-logo.png`)"
+ :key="'HandyCart Logo'"
+            :alt="'HandyCart Logo'"
+            :title="'HandyCart Logo'"
+            class="rounded"
+ />
+ <div>
+  <h1 class="m-0">HandyCart</h1>
+  <!-- <p class="m-0">Since 2022</p> -->
+ </div>
+          </div>
+
+
+          <div class="navigation">
+            <ul class="menu">
+              <li class="nav__item">
+                 <NuxtLink class="" to="/" exact>Home</NuxtLink>
+              </li>
+              <li class="nav__item">
+                 <NuxtLink class="" to="/shop" >Shop</NuxtLink>
+              </li>
+            </ul>
+          </div>
+
+
+          <div class="nav__icons">
+            <span class="cart__icon">
+              <BIconBag/>
+            </span>
+            <span class="cart__icon">
+              <BIconBag/>
+            </span>
+            <span class="cart__icon">
+              <BIconBag/>
+            </span>
+             </div>
+
+
+             <!-- Mobile Menu -->
+             <div class="mobile__menu">
+              <span> <BIconThreeDotsVertical/></span>
+             </div>
+        </div>
+      </div>
+    </div>
+   </div>
   </div>
 </template>
 
 <script>
+
+import { BIconBag,BIconThreeDotsVertical} from "bootstrap-vue";
 export default {
+  components: { BIconBag,BIconThreeDotsVertical},
 
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,98 @@
 <template>
  <div>
-    <h1>Handycart main</h1>
+    
 
-     <LazyImage 
+     <!-- <LazyImage 
  :src="`https://images5.alphacoders.com/532/532559.jpg`"
  :key="'test-image'"
             style="width:500px;height:500px;"
             :alt="'test-image'"
             :title="'test-image'"
             class="rounded"
+ /> -->
+
+ <section class="hero_section">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="hero__content">
+
+          <p class="m-0 hero__subtitle">Trending products in {{getCurrYear}}</p>
+            <h2>Choose , Shop , Wear as per your need</h2>
+            <p class="m-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci quis dolor, officiis facere illum voluptate excepturi incidunt expedita possimus enim!</p>
+
+            <button class="buy__btn"> <NuxtLink class="" to="/shop" >SHOP NOW</NuxtLink></button>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 col-lg-6">
+        <div class="hero__img">
+            <LazyImage 
+ :src="require(`~/assets/images/hero-img.png`)"
+ :key="'HandyCart Hero image'"
+            :alt="'HandyCart Hero image'"
+            :title="'HandyCart Hero image'"
+            class="rounded"
  />
+        </div>
+      </div>
+    </div>
+  </div>
+
+ </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'HandiCart Home',
+  computed:{
+    getCurrYear(){
+      return new Date().getFullYear()
+    }
+  }
 }
 </script>
+
+<style scoped>
+
+.hero_section{
+  background: var(--hero-bg);
+}
+.hero__content{
+  padding-top: 45px;
+}
+.hero__content h2{
+  color: var(--primary-color);
+  font-size:2.5rem;
+  font-weight:600px;
+  margin:20px 0;
+}
+.hero__content p{
+  color: var(--primary-color);
+  line-height: 28px;
+}
+.hero_section p:nth-child(1){
+  font-weight: 500;
+}
+
+.buy__btn{
+  border: none;
+  outline: none;
+  padding: 8px 20px;
+  border-radius: 5px;
+  background: var(--primary-color);
+  color:#fff;
+  cursor: pointer;
+  font-size: 1rem;
+  margin-top: 40px;
+}
+.buy__btn a{
+  color:inherit
+}
+.buy__btn a:hover{
+  color:inherit;
+  text-decoration: none;
+}
+
+
+</style>
